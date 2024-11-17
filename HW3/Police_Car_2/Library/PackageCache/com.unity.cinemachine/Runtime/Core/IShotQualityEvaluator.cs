b355c543f194a499553dd7a9998c8e0ed503289e
Bahareh Kavousi nejad <107205773+Bahareh0281@users.noreply.github.com>
2024-11-18 01:05:44 +0330

    deleted

--- HW3/Police_Car_2/Library/PackageCache/com.unity.cinemachine/Runtime/Core/IShotQualityEvaluator.cs
+++
@@ -1,9 +0,0 @@
-namespace Unity.Cinemachine
-{
-    /// <summary>
-    /// An abstract representation of a component that evaluates shot quality.
-    /// Component (usually a CinemachineExtension) is expected to set state.ShotQuality
-    /// late in the camera pipeline (after Aim).
-    /// </summary>
-    public interface IShotQualityEvaluator {}
-}
