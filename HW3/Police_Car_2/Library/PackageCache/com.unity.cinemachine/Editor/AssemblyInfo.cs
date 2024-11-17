64189891fde92872daf3a4fa2d5ee8f386a0dfa3
Bahareh Kavousi nejad <107205773+Bahareh0281@users.noreply.github.com>
2024-11-17 23:24:58 +0330

    Why?

--- HW3/Police_Car_2/Library/PackageCache/com.unity.cinemachine/Editor/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("Unity.Cinemachine.Shared.Tests")]
-[assembly: InternalsVisibleTo("Unity.Cinemachine.Editor.Tests")]
-[assembly: InternalsVisibleTo("Unity.Cinemachine.Runtime.Tests")]
